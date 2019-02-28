ed661f554a39516465b618bd46ef08acf7379b48
savano miatama <savano.miatama@uisi.ac.id>
2019-02-28 22:23:31 +0700

    Database class

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -1,23 +1,39 @@
  <div class="container-fluid">
 
-                <!-- Page Heading -->
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">
-                            Blank Page
-                            <small>Subheading</small>
-                        </h1>
-                        <ol class="breadcrumb">
-                            <li>
-                                <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
-                            </li>
-                            <li class="active">
-                                <i class="fa fa-file"></i> Blank Page
-                            </li>
-                        </ol>
-                    </div>
-                </div>
-                <!-- /.row -->
+    <!-- Page Heading -->
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">
+                Admin
+                <small>Subheading</small>
+            </h1>
 
-            </div>
-            <!-- /.container-fluid -->
+
+            <?php 
+
+
+            $sql = "SELECT * FROM users WHERE usr_id = 1";
+            $result = $database->query_db($sql);
+            $user_found = mysqli_fetch_array($result);
+
+            echo $user_found['usr_username'];
+
+            ?>
+
+
+
+
+            <ol class="breadcrumb">
+                <li>
+                    <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
+                </li>
+                <li class="active">
+                    <i class="fa fa-file"></i> Blank Page
+                </li>
+            </ol>
+        </div>
+    </div>
+    <!-- /.row -->
+
+</div>
+<!-- /.container-fluid -->
---
+++ admin/includes/init.php
@@ -0,0 +1,8 @@
+<?php 
+
+include("config_db.php"); //database constant
+include("database.php"); //database connection
+
+
+
+?>
